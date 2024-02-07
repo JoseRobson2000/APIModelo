d4ecf440a3f4fbe3a974b21b3cbe881d573b6218
JoseRobson2000 <129907542+JoseRobson2000@users.noreply.github.com>
2024-02-06 22:17:46 -0300

    API com dados

---
+++ APIModelo/Controllers/PessoaController.cs
@@ -0,0 +1,30 @@
+﻿using APIModelo.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIModelo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        [HttpGet("GetPessoa")]
+        public Pessoa GetPessoa()
+        { 
+           Pessoa vPessoa = new Pessoa();
+            vPessoa.Id = 1;
+            vPessoa.Nome = "José Robson";
+            vPessoa.NumeroCelular = "(18)91234-5678";
+
+            return vPessoa;
+        }
+
+        [HttpGet("GetPessoas")]
+        public List<Pessoa> GetPessoas()
+        {
+            var vPessoa = new Pessoa();
+
+            return vPessoa.Dados();
+        }
+    }
+}
---
+++ APIModelo/Model/Pessoa.cs
@@ -0,0 +1,35 @@
+﻿namespace APIModelo.Model
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NumeroCelular { get; set; }
+        public DateOnly DataNascimento { get; set; }
+
+        //Método contendo todos os dados
+        public List<Pessoa> Dados()
+        {
+
+            var ListaPessoa = new List<Pessoa>();
+
+            var vPessoa1 = new Pessoa();
+            vPessoa1.Id = 1;
+            vPessoa1.Nome = "João";
+
+            var vPessoa2 = new Pessoa();
+            vPessoa2.Id = 2;
+            vPessoa2.Nome = "Ana";
+
+            var vPessoa3 = new Pessoa();
+            vPessoa3.Id = 2;
+            vPessoa3.Nome = "Maria";
+
+            ListaPessoa.Add(vPessoa1);
+            ListaPessoa.Add(vPessoa2);
+            ListaPessoa.Add(vPessoa3);
+
+            return ListaPessoa;
+        }
+    }
+}
